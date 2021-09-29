2affbf58ea0ff83752188b6d783bda44fa5bcdde
Daniel da Costa Vitoriano <daniel.costaht2@gmail.com>
2021-09-29 20:54:11 -0300

    finished

--- ATV2_V1/atv2_v1.js
+++ ATV2_V1/atv2_v1.js
@@ -2,6 +2,7 @@
 var students = [];
 //variável de controle
 let clicks = 0;
+let edit_student_index = 0;
 //array dos cursos e cadeiras
 var coursers = [];
 
@@ -55,16 +56,26 @@ function set_radio(reg1, reg2, reg3, reg4){
 function set_selected_chairs(){
     let chairs = [];
     let count_children = document.getElementById("checkboxes_chairs").childElementCount;
-    let childrens = document.getElementById("checkboxes_chairs").children;
+    /*let childrens = document.getElementById("checkboxes_chairs").children;
     let count_inputs = 0;
-    let selected_courser = document.getElementById("select_courses").value;
 
     for(let i = 0; i < count_children; i++){
         if(childrens[i].checked){
             chairs.push(coursers[selected_courser]['chair_' + count_inputs]);
             count_inputs ++;
+        } 
+    }*/
+    count_children / 2;
+    let selected_courser = document.getElementById("select_courses").value;
+    for(let x = 0; x < count_children; x++){
+        if(document.getElementById('chair_1').checked){
+            chairs.push(coursers[selected_courser].chair_0);
+        }
+        if(document.getElementById('chair_2').checked){
+            chairs.push(coursers[selected_courser].chair_1);
         }
     }
+
     return chairs;
 }
 
@@ -92,30 +103,58 @@ function set_default(){
     document.getElementById('reg_modality3').checked = false;
     document.getElementById('reg_modality4').checked = false;
     document.getElementById('select_courses').value = null;
+
+    let count_chairs = document.getElementById('checkboxes_chairs').childElementCount;
+    let chairs_children = document.getElementById('checkboxes_chairs').children;
+    for(let x = 0; x < count_chairs; x ++){
+        if(chairs_children[x].type == "checkbox"){
+            chairs_children[x].checked = false;
+        }
+    }
+
 }
 
-//Cadastro
+//Cadastro e alteração
 function include_in_students(){
-    let reg_name = document.getElementById('reg_name').value;
-    let reg_reg = document.getElementById('reg_reg').value;
-    let reg_date = document.getElementById('reg_date').value;
+    let mod = document.getElementById('button_form1').getAttribute('mod');
+    if(mod == 1){
+        //cadastrar
+        let reg_name = document.getElementById('reg_name').value;
+        let reg_reg = document.getElementById('reg_reg').value;
+        let reg_date = document.getElementById('reg_date').value;
 
-    let reg_reg_date = document.getElementById("reg_reg_date").value;
-    let reg_modality = set_radio(document.getElementById("reg_modality1"), document.getElementById("reg_modality2"), document.getElementById("reg_modality3"), document.getElementById("reg_modality4"));
-    let reg_courser = set_courser();
+        let reg_reg_date = document.getElementById("reg_reg_date").value;
+        let reg_modality = set_radio(document.getElementById("reg_modality1"), document.getElementById("reg_modality2"), document.getElementById("reg_modality3"), document.getElementById("reg_modality4"));
+        let reg_courser = set_courser();
 
-    let reg_chair = set_selected_chairs();
+        let reg_chair = set_selected_chairs();
 
-    students.push( new student(reg_name, reg_reg, reg_date, reg_reg_date, reg_courser, reg_modality, reg_chair)); //inserindo o novo cadastro no final do array
+        students.push( new student(reg_name, reg_reg, reg_date, reg_reg_date, reg_courser, reg_modality, reg_chair)); //inserindo o novo cadastro no final do array
+
+        reg_chair = null;
+        reg_courser = null;
+        alert("Estudante cadastrado com sucesso!");
+    }
+    else if(mod == 2){
+        students[edit_student_index].student_name = document.getElementById('reg_name').value;
+        students[edit_student_index].student_reg = document.getElementById('reg_reg').value;
+        students[edit_student_index].student_date = document.getElementById('reg_date').value;
+        students[edit_student_index].student_reg_date = document.getElementById('reg_reg_date').value;
+
+        students[edit_student_index].student_modality = set_radio(document.getElementById("reg_modality1"), document.getElementById("reg_modality2"), document.getElementById("reg_modality3"), document.getElementById("reg_modality4"));
+
+        students[edit_student_index].student_course = set_courser();
+        students[edit_student_index].student_chairs = set_selected_chairs();
+
+        alert("Dados alterado com sucesso!");
+    }
 
     set_default();
-
-    reg_chair = null;
-    reg_courser = null;
-    alert("Estudante cadastrado com sucesso!");
 }
 //leitura / exibição lista / edit aluno
 function show_students_list(val1, val2, table_id){
+    document.getElementById("search_reg").value = null;
+
     let show_students_body = document.getElementById(table_id);
     show_students_body.innerText = '';
     if(val1 == null && val2 == null){
@@ -166,15 +205,40 @@ function show_students_list(val1, val2, table_id){
         
     }
 }
-//função que pega os dados no array
+//função que pega os dados no array e insere no form
 function get_student(student_index){
+    edit_student_index = student_index;
     on_cad_click('div_add');
     document.getElementById('reg_name').value = students[student_index].student_name;
     document.getElementById('reg_reg').value = students[student_index].student_reg;
     document.getElementById('reg_date').value = students[student_index].student_date;
     document.getElementById("reg_reg_date").value = students[student_index].student_reg_date;
-    //passar os valores de chair e courser
+    //
 
+    let count_modality = document.getElementById('modality').childElementCount;
+    count_modality = count_modality / 2 - 1;
+    for(let x = 1; x < count_modality; x ++){
+        let label_modality = document.getElementById('label_modality' + x).getAttribute('valor');
+        if(students[student_index].student_modality == label_modality){
+            document.getElementById('reg_modality' + x).checked = true;
+        }
+    }
+    let courser_value;
+    for(let x = 0; x < coursers.length; x++){
+        for(let z = 0; z < students[student_index].student_chairs.length; z++){
+            if(coursers[x].chair_0 == students[student_index].student_chairs[z]){
+                document.getElementById('chair_1').checked = true;
+                courser_value = x;
+            }
+            if(coursers[x].chair_1 == students[student_index].student_chairs[z]){
+                document.getElementById('chair_2').checked = true;
+                courser_value = x;
+            }
+        }
+    }
+    document.getElementById('select_courses').value = courser_value;
+
+    document.getElementById('button_form1').setAttribute('mod', 2);
 }
 
 
@@ -203,6 +267,7 @@ function chairs_opt(){
 }
 // as 2 funções a seguir esconde e exibi as 2 divs principais
 function on_cad_click(el){
+    document.getElementById('button_form1').setAttribute('mod', 1);
     var display = document.getElementById(el).style.display;
         if(display == "none"){
             document.getElementById(el).style.display = 'block';
@@ -227,7 +292,4 @@ function on_list_click(el){
 //hidden automático das 2 divs principais
 function hidden(el){
     document.getElementById(el).style.display = "none";
-}
-function teste(){
-    console.log('ihu ihu');
 }
